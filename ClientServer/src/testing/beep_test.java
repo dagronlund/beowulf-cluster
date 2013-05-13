6385be101a2a4be9d6ee612048004c812b652d90
Adam Bell <abellnets@gmail.com>
2013-05-13 16:23:20 -0600

    Comment... abellnets@gmail.com 	renamed:    ClientServer/src/main/ClientTest.java -> ClientServer/src/testing/ClientTest.java 	renamed:    ClientServer/src/main/ServerTest.java -> ClientServer/src/testing/ServerTest.java 	new file:   ClientServer/src/testing/beep_test.java

---
+++ ClientServer/src/testing/beep_test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+/**
+ *
+ * @author Adam Bell
+ */
+public class beep_test {
+    public static void main(String[] args){
+        while(true){
+//            java.awt
+        }
+    }
+}
